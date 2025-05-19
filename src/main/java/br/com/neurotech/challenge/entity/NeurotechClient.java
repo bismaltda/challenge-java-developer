ceba1b417da66d9836b347a88cb51fad24e61aa8
Bismarck Pereira <bismarck.pereira@mv.com.br>
2025-05-19 11:52:18 -0300

    Criando as primeiras estruturas funcionais do projeto.

--- src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
+++ src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
@@ -1,12 +1,31 @@
 package br.com.neurotech.challenge.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "client")
 public class NeurotechClient {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private Long id;
+
+	@Column(nullable = false, length = 150)
 	private String name;
+
+	@Column(nullable = false)
 	private Integer age;
+
+	@Column(nullable = false)
 	private Double income;
 
 }
--- src/main/java/br/com/neurotech/challenge/service/CreditService.java
+++ src/main/java/br/com/neurotech/challenge/service/CreditService.java
@@ -5,12 +5,8 @@ import org.springframework.stereotype.Service;
 import br.com.neurotech.challenge.entity.VehicleModel;
 
 @Service
-public interface CreditService {
+public class CreditService {
 	
-	/**
-	 * Efetua a checagem se o cliente está apto a receber crédito
-	 * para um determinado modelo de veículo
-	 */
-	boolean checkCredit(String clientId, VehicleModel model);
+
 	
 }
